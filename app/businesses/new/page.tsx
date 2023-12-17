fe8a15301b419f58a898bb054ca491bec51003d6
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-17 19:48:46 +0200

    Extract the ErrorMessage component

--- app/businesses/new/page.tsx
+++ app/businesses/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createBusinessSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type BusinessForm = z.infer<typeof createBusinessSchema>;
 
@@ -46,11 +47,7 @@ const newBusinessPage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -58,11 +55,7 @@ const newBusinessPage = () => {
             <SimpleMDE placeholder="Desciption" {...field}></SimpleMDE>
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Business</Button>
       </form>
     </div>
