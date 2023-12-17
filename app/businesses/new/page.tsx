8746c9efa34d0ddb21ea5fe3a4dfdefa6973a7c2
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-17 18:55:11 +0200

    Add a markdown Editor

--- app/businesses/new/page.tsx
+++ app/businesses/new/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const newBusinessPage = () => {
   return (
@@ -9,7 +10,7 @@ const newBusinessPage = () => {
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Desciption"></TextArea>
+      <SimpleMDE placeholder="Desciption"></SimpleMDE>
       <Button>Submit New Business</Button>
     </div>
   );
