152ec3bf633eb49a662d6cd2030cbc8d8af272d7
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-17 17:28:38 +0200

    Style the active link in the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,16 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBusinessTime } from "react-icons/fa";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "businesses", href: "/businesses" },
+    { label: "Businesses", href: "/businesses" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -16,7 +21,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li key={link.href}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800": true,
+              })}
               href={link.href}
             >
               {link.label}
---
+++ app/businesses/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const businessesPage = () => {
+  return <div>businessesPage</div>;
+};
+
+export default businessesPage;
