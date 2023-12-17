ff8400baf230ecf48391d9d0ac88a61d429680a4
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-17 19:20:47 +0200

    Handle Errors

--- app/api/businesses/route.ts
+++ app/api/businesses/route.ts
@@ -4,8 +4,8 @@ import prisma from "@/prisma/client";
 
 // Validation Schema
 const createBusinessSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required."),
 });
 
 export async function POST(request: NextRequest) {
@@ -14,7 +14,7 @@ export async function POST(request: NextRequest) {
   // Validate Response
   const validation = createBusinessSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   // Store Business in database
   const newBusiness = await prisma.business.create({
--- app/businesses/new/page.tsx
+++ app/businesses/new/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface BusinessForm {
   title: string;
@@ -15,27 +16,39 @@ interface BusinessForm {
 const newBusinessPage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<BusinessForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/businesses", data);
-        router.push("/businesses");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Desciption" {...field}></SimpleMDE>
-        )}
-      />
-      <Button>Submit New Business</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/businesses", data);
+            router.push("/businesses");
+          } catch (error) {
+            setError("An unexpected error occured.");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Desciption" {...field}></SimpleMDE>
+          )}
+        />
+        <Button>Submit New Business</Button>
+      </form>
+    </div>
   );
 };
 
