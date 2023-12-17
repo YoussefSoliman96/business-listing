d7dacc0b006b202a8d2b0ba19bf8f876b451aa4e
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-17 19:44:29 +0200

    Implement client-side validation

--- app/businesses/new/page.tsx
+++ app/businesses/new/page.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createBusinessSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface BusinessForm {
-  title: string;
-  description: string;
-}
+type BusinessForm = z.infer<typeof createBusinessSchema>;
 
 const newBusinessPage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<BusinessForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BusinessForm>({
+    resolver: zodResolver(createBusinessSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -39,6 +46,11 @@ const newBusinessPage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -46,6 +58,11 @@ const newBusinessPage = () => {
             <SimpleMDE placeholder="Desciption" {...field}></SimpleMDE>
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Business</Button>
       </form>
     </div>
