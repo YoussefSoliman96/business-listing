daa467e159acc1340be3525457557ed573a12058
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-17 21:27:38 +0200

    Refactor: Move inline function

--- app/api/businesses/route.ts
+++ app/api/businesses/route.ts
@@ -5,12 +5,10 @@ import { createBusinessSchema } from "../../validationSchemas";
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
-  // Validate Response
   const validation = createBusinessSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
-  // Store Business in database
   const newBusiness = await prisma.business.create({
     data: { title: body.title, description: body.description },
   });
