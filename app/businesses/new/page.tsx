ee9528aae716dd9e0fa67b43d3066d37c521c64b
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-17 19:08:09 +0200

    Handle form submission

--- app/businesses/new/page.tsx
+++ app/businesses/new/page.tsx
@@ -2,17 +2,40 @@
 
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+interface BusinessForm {
+  title: string;
+  description: string;
+}
 
 const newBusinessPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<BusinessForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/businesses", data);
+        router.push("/businesses");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Desciption"></SimpleMDE>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Desciption" {...field}></SimpleMDE>
+        )}
+      />
       <Button>Submit New Business</Button>
-    </div>
+    </form>
   );
 };
 
