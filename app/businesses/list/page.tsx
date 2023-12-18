ce9466c442ec135c67f348171e96e3f900301a31
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-19 01:20:15 +0200

    Make columns sortable

--- app/businesses/list/page.tsx
+++ app/businesses/list/page.tsx
@@ -1,10 +1,27 @@
 import prisma from "@/prisma/client";
 import { Badge, Table } from "@radix-ui/themes";
 import { Link } from "@/app/components";
+import NextLink from "next/link";
 import BusinessActions from "./BusinessActions";
+import { Business } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
-const businessesPage = async () => {
+interface Props {
+  searchParams: { orderBy: keyof Business };
+}
+
+const businessesPage = async ({ searchParams }: Props) => {
   const businesses = await prisma.business.findMany();
+
+  const columns: {
+    label: string;
+    value: keyof Business;
+    className?: string;
+  }[] = [
+    { label: "Business", value: "title" },
+    { label: "Email", value: "email", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
 
   return (
     <div>
@@ -12,13 +29,19 @@ const businessesPage = async () => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Business</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Owner Email
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
+                <NextLink href={`/businesses/list?orderBy=${column.value}`}>
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -31,7 +54,7 @@ const businessesPage = async () => {
                 <div className="block md:hidden">email</div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <Badge>{business.email}</Badge>
+                <Badge color="gray">{business.email}</Badge>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {business.createdAt.toString()}
