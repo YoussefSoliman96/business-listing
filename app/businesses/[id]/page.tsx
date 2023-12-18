45d12542ba55b911e170c7c7fb6bbf10d18700d4
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-18 06:14:19 +0200

    Add the edit button

--- app/businesses/[id]/page.tsx
+++ app/businesses/[id]/page.tsx
@@ -1,7 +1,18 @@
 import prisma from "@/prisma/client";
-import { Badge, Card, Flex, Heading, Text } from "@radix-ui/themes";
+import {
+  Badge,
+  Box,
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Heading,
+  Text,
+} from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
@@ -15,16 +26,24 @@ const BusinessDetailPage = async ({ params }: Props) => {
   if (!business) notFound();
 
   return (
-    <div>
-      <Heading>{business.title}</Heading>
-      <Flex className="gap-3">
-        <Badge color="blue">Email</Badge>
-        <Text>{business.createdAt.toString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{business.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{business.title}</Heading>
+        <Flex className="gap-3">
+          <Badge color="blue">Email</Badge>
+          <Text>{business.createdAt.toString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{business.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/businesses/${business.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
