1326b2f884f25defe7dde0b6e3fdfd1e0f0342a3
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-19 06:04:53 +0200

    Revamp prisma

--- app/components/OwnerEmailBadge.tsx
+++
@@ -1,8 +0,0 @@
-import { Badge } from "@radix-ui/themes";
-import React from "react";
-
-const OwnerEmailBadge = () => {
-  return <Badge color="blue">Email</Badge>;
-};
-
-export default OwnerEmailBadge;
