23bcfa5e5ff27c1a8c4b87f0d4d72af28c08c1d1
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-19 04:42:35 +0200

    Clear database

--- prisma/migrations/20231218204423_add_business_owner/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Business` ADD COLUMN `ownerId` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Business` ADD CONSTRAINT `Business_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20231219024217_initial_migration/migration.sql
+++ prisma/migrations/20231219024217_initial_migration/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE `Business` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `email` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Business_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Account` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
